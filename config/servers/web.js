7db03d23eaea4cb0cc923fad656882190b69d0cf
Evan Tahler <evantahler@gmail.com>
2016-02-12 16:38:59 -0800

    actionhero version 13

--- config/servers/web.js
+++ config/servers/web.js
@@ -8,7 +8,7 @@ exports.default = {
         // Passed to https.createServer if secure=true. Should contain SSL certificates
         serverOptions: {},
         // Port or Socket Path
-        port: 8080,
+        port: process.env.PORT || 8080,
         // Which IP to listen on (use '0.0.0.0' for all; '::' for all on ipv4 and ipv6)
         // Set to `null` when listening to socket
         bindIP: '0.0.0.0',
@@ -24,7 +24,7 @@ exports.default = {
         urlPathForActions : 'api',
         // Route that static files will be served from;
         //  path (relative to your project root) to serve static content from
-        //  set to `null` to disable the file server entirely 
+        //  set to `null` to disable the file server entirely
         urlPathForFiles : 'public',
         // When visiting the root URL, should visitors see 'api' or 'file'?
         //  Visitors can always visit /api and /public as normal
@@ -35,12 +35,18 @@ exports.default = {
         queryRouting : true,
         // The header which will be returned for all flat file served from /public; defined in seconds
         flatFileCacheDuration : 60,
+        // How many times should we try to boot the server?
+        // This might happen if the port is in use by another process or the socketfile is claimed
+        bootAttempts: 1,
         // Settings for determining the id of an http(s) request (browser-fingerprint)
         fingerprintOptions : {
           cookieKey: 'sessionID',
           toSetCookie: true,
           onlyStaticElements: false,
-          settings: 'path=/;'
+          settings: {
+            path: '/',
+            expires: 3600000,
+          }
         },
         // Options to be applied to incoming file uploads.
         //  More options and details at https://github.com/felixge/node-formidable
@@ -60,15 +66,18 @@ exports.default = {
         // When true, returnErrorCodes will modify the response header for http(s) clients if connection.error is not null.
         //  You can also set connection.rawConnection.responseHttpCode to specify a code per request.
         returnErrorCodes: true,
-        // How many times should we try to boot the srever?
-        // This might happen if the port is in use by another process or the socketfile is claimed
-        bootAttempts: 3,
-      }
+        // should this node server attempt to gzip responses if the client can accept them?
+        // this will slow down the performance of actionhero, and if you need this funcionality, it is recommended that you do this upstream with nginx or your load balancer
+        compress: false,
+        // options to pass to the query parser
+        // learn more about the options @ https://github.com/hapijs/qs
+        queryParseOptions: {},
+      };
     }
   }
-}
+};
 
-exports.production = { 
+exports.production = {
   servers: {
     web: function(api){
       return {
@@ -78,12 +87,12 @@ exports.production = {
           serverInformation: false,
           requesterInformation: false
         }
-      }
+      };
     }
   }
-}
+};
 
-exports.staging = { 
+exports.staging = {
   servers: {
     web: function(api){
       return {
@@ -93,13 +102,13 @@ exports.staging = {
           serverInformation: false,
           requesterInformation: false
         }
-      }
+      };
     }
   }
-}
+};
 
 exports.test = {
-  servers: { 
+  servers: {
     web: function(api){
       return {
         secure: false,
@@ -109,7 +118,7 @@ exports.test = {
           serverInformation: true,
           requesterInformation: true
         }
-      }
+      };
     }
   }
-}
+};
--- tasks/check.js
+++ tasks/check.js
@@ -5,13 +5,15 @@ exports.task = {
   queue:         'statusbot',
   plugins:       [],
   pluginOptions: {},
-  
+
   run: function(api, params, next){
+    var delay = 3000; // need to sleep for statuspage.io rate limit [ http://doers.statuspage.io/api/v1/ ]
+
     api.check.check(params.url, function(err, details){
       setTimeout(function(){
         api.tasks.enqueue("check", params, 'statusbot');
         next(err, details);
-      }, 3000); // need to sleep for statuspage.io rate limit [ http://doers.statuspage.io/api/v1/ ]
+      }, delay); 
     });
   }
 };
